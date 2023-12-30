513fe126a9e06e92d0a6166b2e93ad7ec0b48c1a
AndoniHarambillet <andoni.harambillet@net.estia.fr>
2023-12-30 02:55:25 +0100

    [WIP] Adding material.io library to get a UI

--- victoria-angular/src/app/app.module.ts
+++ victoria-angular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryStatsDisplayComponent } from './ui/country-stats-display/country-stats-display.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryStatsDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ victoria-angular/src/app/ui/country-stats-display/country-stats-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-stats-display',
+  templateUrl: './country-stats-display.component.html',
+  styleUrls: ['./country-stats-display.component.scss']
+})
+export class CountryStatsDisplayComponent {
+
+}
